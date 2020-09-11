4628ac9dfdff8f7fc57409e335a3143c3eb6b0b6
yeongbba <yeongmolee2@gmail.com>
2020-09-11 19:25:46 +0900

    my first version

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
-window.addEventListener("load", () => {
+const commentBox = document.querySelector(".comment");
+const tweetbtn = document.querySelector(".tweet__btn");
+const refreshBtn = document.querySelector(".refresh__btn");
+const gobackBtn = document.querySelector(".goback__btn");
+
+function createBaseComment() {
   for (let i = 0; i < DATA.length; i++) {
     let template = document.querySelector("template");
     let cloneNode = document.importNode(template.content, true);
     let spans = cloneNode.querySelectorAll("span");
-    let commentBox = document.querySelector(".comment");
 
     spans[0].textContent = DATA[i].user;
     spans[1].textContent = DATA[i].created_at;
@@ -12,17 +16,14 @@ window.addEventListener("load", () => {
     commentBox.appendChild(cloneNode);
   }
   addClickUserEvent();
-});
+}
 
-let tweetbtn = document.querySelector(".tweet__btn");
-
-tweetbtn.addEventListener("click", () => {
+function createNewTweet() {
   let template = document.querySelector("template");
   let cloneNode = document.importNode(template.content, true);
   let spans = cloneNode.querySelectorAll("span");
   let usernameInput = document.querySelector(".tweet__username input");
-  let messageInput = document.querySelector(".tweet__message input");
-  let commentBox = document.querySelector(".comment");
+  let messageInput = document.querySelector(".tweet__message textarea");
 
   spans[0].textContent = usernameInput.value;
   spans[1].textContent = new Date().format();
@@ -30,51 +31,53 @@ tweetbtn.addEventListener("click", () => {
 
   commentBox.prepend(cloneNode);
   addClickUserEvent();
-});
-
-function InsertToArray() {
-  let tweetArr = [];
-  for (let i = 0; i < 8; i++) {
-    tweetArr.push(generateNewTweet());
-  }
-  return tweetArr;
 }
 
-let refreshBtn = document.querySelector(".refresh__btn");
+function refreshComment() {
+  let template = document.querySelector("template");
+  let cloneNode = document.importNode(template.content, true);
+  let spans = cloneNode.querySelectorAll("span");
+  let newTweet = generateNewTweet();
 
-refreshBtn.addEventListener("click", () => {
-  let commentRows = document.querySelectorAll(".comment__row");
-  for (let i = 0; i < commentRows.length; i++) {
-    commentRows[i].remove();
-  }
+  spans[0].textContent = newTweet.user;
+  spans[1].textContent = newTweet.created_at;
+  spans[2].textContent = newTweet.message;
 
-  let tweetArr = InsertToArray();
-  for (let i = 0; i < tweetArr.length; i++) {
-    let template = document.querySelector("template");
-    let cloneNode = document.importNode(template.content, true);
-    let spans = cloneNode.querySelectorAll("span");
-    let commentBox = document.querySelector(".comment");
-
-    spans[0].textContent = tweetArr[i].user;
-    spans[1].textContent = tweetArr[i].created_at;
-    spans[2].textContent = tweetArr[i].message;
-
-    commentBox.append(cloneNode);
-  }
+  commentBox.prepend(cloneNode);
   addClickUserEvent();
-});
+}
+
+function goBackToList() {
+  let comment = document.querySelectorAll(".comment__row");
+  console.log(comment);
+  for (let i = 0; i < comment.length; i++) {
+    comment[i].classList.remove("display__none");
+  }
+  refreshBtn.classList.remove("display__none");
+  gobackBtn.classList.add("display__none");
+}
 
 function addClickUserEvent() {
   let userArr = document.querySelectorAll(".comment__user");
+
   for (let i = 0; i < userArr.length; i++) {
     userArr[i].addEventListener("click", (e) => {
       for (let j = 0; j < userArr.length; j++) {
         if (e.target.textContent !== userArr[j].textContent) {
-          userArr[j].parentElement.parentElement.remove();
+          userArr[j].parentElement.parentElement.classList.add("display__none");
         }
       }
+      refreshBtn.classList.add("display__none");
+      gobackBtn.classList.remove("display__none");
     });
   }
 }
 
-//document.getElementById('test').innerHTML = 'hello twittler, check developer console!';
+function init() {
+  window.addEventListener("load", createBaseComment);
+  tweetbtn.addEventListener("click", createNewTweet);
+  refreshBtn.addEventListener("click", refreshComment);
+  gobackBtn.addEventListener("click", goBackToList);
+}
+
+init();
